fbc9d9d6fa01cc804cffc2a71141ce4da049fc2b
LabsupportEvert <98744988+LabsupportEvert@users.noreply.github.com>
2023-07-27 11:40:35 +0200

    InitialFirmwareRelease

--- Firmware/Manometer/Manometer.ino
+++ Firmware/Manometer/Manometer.ino
@@ -1,10 +1,15 @@
+/*  Pressure Sensor to measure suction and controle pump
+    University Leiden Fsw-Solo Evert 2022
 
+   20221116 1.0 Beta test hardware concept. 
+*/
+  
 
 
 #include "ADS1X15.h"  //https://github.com/RobTillaart/ADS1X15
 
 
-ADS1115 ADS(0x48);
+ADS1115 ADS(0x48);  //I2c address
 #define SW_auto 12
 #define SW_manual 13
 #define SPEEDPOT 0
@@ -12,34 +17,80 @@ ADS1115 ADS(0x48);
 #define MANUAL 1
 #define AUTO 2
 #define OFF 0
+#define UPPERBOUNDERY 100
+#define LOWERBOUNDERY 100
+#define PUMPTIME 1000 //msec
+
+int ZeroOffset = 0;
+boolean PumpIsRunning = false;
+unsigned long StartTime = 0;
+String Pumping = "U" ;
 
 void setup()
 {
   pinMode(SW_manual, INPUT_PULLUP);
   pinMode(SW_auto, INPUT_PULLUP);
   Serial.begin(115200);
-  Serial.println(__FILE__);
-  Serial.print("ADS1X15_LIB_VERSION: ");
-  Serial.println(ADS1X15_LIB_VERSION);
-
   ADS.begin();
   ADS.setGain(16);      // 0.256V volt
   ADS.setDataRate(7);  // fast
   ADS.setMode(0);      // continuous mode
   ADS.readADC(0);      // first read to trigger
+  ZeroOffset = CalibrateZero();
 }
 
 
 void loop()
 {
-  Serial.println(ADS.readADC_Differential_2_3());
-  // optional other code here
-  //delay(100);
- analogWrite(PUMP, analogRead(SPEEDPOT)>> 2);
+  int PressureValue = ADS.readADC_Differential_2_3();
+  Serial.print(millis());
+  Serial.print(";");
+  Serial.print(Pumping);
+  Serial.print(";");
+  Serial.println(PressureValue);
+
+  switch (checkswitch()) {
+    case OFF:
+      PumpPwm( 0);
+      break;
+    case MANUAL:
+     PumpPwm(analogRead(SPEEDPOT) >> 2);
+      break;
+    case AUTO:
+      if ((PressureValue < ZeroOffset - LOWERBOUNDERY) || (PressureValue > ZeroOffset + UPPERBOUNDERY)) {
+        StartTime = millis();
+        if (PumpIsRunning == false) {
+          PumpIsRunning = true;
+        }
+      }
+      break;
+    default:
+      break;
+  }
+
+  if (PumpIsRunning == true) {
+    if (StartTime + PUMPTIME > millis()) {
+      PumpPwm(analogRead(SPEEDPOT) >> 2);
+    }
+    else {
+      PumpPwm(0);
+      PumpIsRunning = false;
+    }
+  }
+
+}
+
+void PumpPwm(byte speed){
+  analogWrite(PUMP, speed);
+  if (speed==0){
+    Pumping="F";
+  }
+  else{
+    Pumping="T";
+  }
 }
 
 
-// -- END OF FILE --
 
 byte checkswitch() {
   if (digitalRead(SW_manual) == false) {
@@ -51,4 +102,13 @@ byte checkswitch() {
   if (digitalRead(SW_manual) && digitalRead(SW_auto)) {
     return OFF;
   }
+}
+
+int CalibrateZero() {
+  int avarage = 0;
+  for (int i = 1; i <= 10; i++) {
+    avarage += ADS.readADC_Differential_2_3();
+    delay(100);
+  }
+  return avarage / 10;
 }
